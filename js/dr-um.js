4ac496314b707a4e43af9fc9294bf4be7540f80b
chemisier-tyran <chemisier.tyran@gmail.com>
2012-12-27 23:38:37 +0100

    hihat to toms implemented async sounds

--- js/dr-um.js
+++ js/dr-um.js
@@ -1,61 +1,66 @@
 $(function () {
 
-    $("#click-me-clap").click(function () {
-        console.log("clap");
-        $("#clap")[0].play();
-    });
-
-    $("#click-me-crash").click(function () {
-        console.log("crash");
-        $("#crash")[0].play();
-    });
+    var i = 0;
+// allows to play HTML5 sounds ASYNCHRONOUSLY (for crash and crush..)
+    function snd(soundname){
+        if(soundname != null){
+            $(soundname).clone()
+                .attr('id', 'audio_clone_'+i)
+                .appendTo('#audio-div');
+            var elem = document.getElementById('audio_clone_'+i);
+            setTimeout("$('#audio_clone_"+i+"').remove();",5000);
+            i++;
+            if(elem){
+                elem.play();
+            }
+        }
+    }
 
     $(document).keyup(function (e) {
         console.log(e.which);
         switch (e.which) {
-            case 65:
-                $("#clap")[0].play();
-                break;
-            case 90:
-                $("#crash")[0].play();
-                break;
-            case 69:
-                $("#hihat")[0].play();
-                break;
-            case 82:
-                $("#kick")[0].play();
+            case 188:
+                snd("#tz");
+//                $("#tz")[0].play();
                 break;
             case 84:
-                $("#snare")[0].play();
+                snd("#tchk");
+//                $("#tchk")[0].play();
                 break;
-            case 89:
-                $("#stick")[0].play();
+            case 32:
+                snd("#poom");
+//                $("#poom")[0].play();
+                break;
+            case 86:
+                snd("#stick");
+//                $("#stick")[0].play();
+                break;
+            case 190:
+                snd("#shaker");
+//                $("#shaker")[0].play();
+                break;
+            case 65:
+                snd("#crash");
+//                $("#crash")[0].play();
+                break;
+            case 90:
+                snd("#crush");
                 break;
             case 85:
-                $("#shaker")[0].play();
+                snd("#tom1");
                 break;
             case 73:
-                $("#snap")[0].play();
+                snd("#tom2");
                 break;
             case 79:
-                $("#tom")[0].play();
+                snd("#tom3");
+                break;
+            case 80:
+                snd("#tom4");
                 break;
 
         }
     });
-
-//    $("#click-me").click( function () {
-//        console.log("clicked");
-//        $("#audio")[0].play();
-//    });
-
-//    $("#hover-me").hover(
-//        function () {
-//            console.log("hover start");
-//            $("#audio")[0].play();
-//        }, function () {
-//            console.log("hover stop");
-//        });
 });
 
 
